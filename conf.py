a2de9a214b2368bac01058c097b02388ee9d33da
Daniele Scasciafratte <mte90net@gmail.com>
2023-06-01 15:43:56 +0200

    fix(home): migrated

--- conf.py
+++ conf.py
@@ -305,7 +305,7 @@ COMPILERS = {
 # Nikola supports logo display.  If you have one, you can put the URL here.
 # Final output is <img src="LOGO_URL" id="logo" alt="BLOG_TITLE">.
 # The URL may be relative to the site root.
-LOGO_URL = '/images/italian_linux_society_round_small.png'
+LOGO_URL = '/images/logo.png'
 
 # When linking posts to social media, Nikola provides Open Graph metadata
 # which is used to show a nice preview. This includes an image preview
